b7154448bafd9aacc452c238dffd2a8aee644337
Rohil Pal <rohilpal9763@gmail.com>
2024-12-21 23:35:51 +0530

    Implement kv store in Redis to store unread_msg_count

--- redis_demos/unread_msg_indicator/lazy-compute/event_subscriber.py
+++ redis_demos/unread_msg_indicator/lazy-compute/event_subscriber.py
@@ -3,12 +3,35 @@
 
 import pika
 import json
+import redis
+from typing import List
+import requests
 
-def callback(ch, method, properties, body):
+BASE_URL = "http://127.0.0.1:8000"
+# create redis client
+redis_client = redis.StrictRedis(host='localhost', port=6379, decode_responses=True)
+
+
+async def callback(ch, method, properties, body):
     message = json.loads(body)
     print(f" [x] Received {message}")
 
     # now here we have to write the logic to update the counter
+    # for any new message from any sender, this function will be called.
+    # these are micro writes to the DB.
+    # TODO: We will also have to keep buffering new messages somewhere and avoid any micro-reads
+    # for v1, we will do microreads
+    url = f"{BASE_URL}/users/update_unread_msg_count"
+    params = {"user_id": message['to'], "new_sender_ids": list(message['from'])}
+
+    # I am using await since I don't want to wait for the request to complete.
+    response = await requests.post(url, params=params)
+    return response
+
+
+#############################
+###### Event consumers ######
+#############################
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
